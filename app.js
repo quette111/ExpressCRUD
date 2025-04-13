3695f899908119792acd398ac5ed40400ae20ac8
Edward Marquette Wilhite <quettehanson@gmail.com>
2025-04-13 08:33:12 -0400

    DELETE

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express()
-const PORT = 3001;
+const PORT = 3002;
 
 const { router } = require('./routers/router.js')
 const logger = (req, res, next) => {
--- controllers/data.js
+++ controllers/data.js
@@ -1,6 +1,6 @@
 const express = require('express');
 
-const { sosaData } = require('../CK.js');
+let { sosaData } = require('../CK.js');
 
 const getFile = (req, res) => {res.json(sosaData)}
 
@@ -10,14 +10,8 @@ const postFile = (req, res) => {sosaData.push(req.body)
 
 const deleteFile = (req, res) => {
 
-const iden = sosaData.find((sosa)=> {sosa.id === id})
-        const isFilt = (num) => {
-            if(num != req.params.id){
-
-        } else {return num != req.params.id}
-    }
- res.send(sosaData.filter(isFilt))
-
+   sosaData = sosaData.filter(song => song.id !== parseInt(req.params.id));
+    res.send(sosaData)
 }
 
 module.exports = { getFile, postFile, deleteFile }
